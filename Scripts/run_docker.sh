2b6caf9a72a350973957552bd9a94c3972ea5a84
Fredrik Magnusson <fredrikmagnusson3@gmail.com>
2023-11-18 15:19:22 +0100

    refactor: :recycle: Refactor dockerfile to include plugins and skip setup
    Also fixed issues with testing inside the jenkins container

--- Scripts/run_docker.sh
+++ Scripts/run_docker.sh
@@ -6,6 +6,15 @@ error_exit()
     exit 1
 }
 
+HOST_IP=$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | cut -d/ -f1 | head -n 1)
+if [ -z "$HOST_IP" ]; then
+    error_exit "Failed to fetch host IP address"
+else
+    echo "Host IP address: $HOST_IP"
+fi
+
+export HOST_IP
+
 chown 1000:1000 ../jenkins-data || error_exit "Failed to change ownership of ../jenkins-data"
 
 chmod 666 /var/run/docker.sock || error_exit "Failed to set permissions on /var/run/docker.sock"
